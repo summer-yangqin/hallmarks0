15d4c533c4175d3ce9328e5dc1949b5734f34c94
tedgoldstein <tedgoldstein@gmail.com>
2017-06-08 17:49:47 -0700

    Only need gene lists here.

--- Signatures/bin/buildSignatures.sh
+++ Signatures/bin/buildSignatures.sh
@@ -2,6 +2,8 @@
 s='Signatures'
 t='tmp'
 
+i='../genelists/GO/data/hallmarks.genes'
+
 mkdir -p $s $t
 for o in  \
     "acute_myeloid_leukemia" \
@@ -10,7 +12,7 @@ for o in  \
     "lung.adenocarcinoma" 
 do
     echo Processing $o
-    for h in `ls hallmarks/`
+    for h in `ls $i/`
     do
 
         # hallmark
@@ -25,13 +27,14 @@ do
 
 
 
-        python bin/matrix_slice.py hallmarks/$h "lists/TCGA.$o"  ref/TCGA.RSEM > "$t/$o.TCGA.$h.data"
+        echo python bin/matrix_slice.py $i/$h "lists/TCGA.$o"  ReferenceData/TCGA.RSEM > "$t/$o.TCGA.$h.data"
+        exit 1
 
-        python bin/matrix_slice.py hallmarks/$h "lists/GTEX.$o"  ref/GTEX.RSEM > "$t/$o.GTEX.$h.data"
+        python bin/matrix_slice.py $i/$h "lists/GTEX.$o"  ReferenceData/GTEX.RSEM > "$t/$o.GTEX.$h.data"
         python bin/matrix_join_common.py "$t/$o.GTEX.$h.data" "$t/$o.TCGA.$h.data" > "$t/$o.GTEX.TCGA.$h.data"
         head -1 "$t/$o.GTEX.TCGA.$h.data" | tr '\t' '\n' | sed -e '1d;s/GTEX.*/0/;s/TCGA.*/1/' > "$t/$o.GTEX.TCGA.$h.phen"
         cut -f1 "$t/$o.GTEX.TCGA.$h.data" | sed -e '1d' > "$t/$o.GTEX.TCGA.$h.genes"
-        python bin/adjacency.py  ref/NETWORK "$t/$o.GTEX.TCGA.$h.genes" | sed -e 's/ //g' > "$t/$o.GTEX.TCGA.$h.network"
+        python bin/adjacency.py  ReferenceData/NETWORK "$t/$o.GTEX.TCGA.$h.genes" | sed -e 's/ //g' > "$t/$o.GTEX.TCGA.$h.network"
 
         Rscript bin/gelNet.R "$t/$o.GTEX.TCGA.$h.data" "$t/$o.GTEX.TCGA.$h.phen" "$t/$o.GTEX.TCGA.$h.network" "$s/$o.GTEX.TCGA.$h.signature" "$h" "$o" \
             echo FAIL Rscript bin/gelNet.R "$t/$o.GTEX.TCGA.$h.data" "$t/$o.GTEX.TCGA.$h.phen" "$t/$o.GTEX.TCGA.$h.network" "$s/$o.GTEX.TCGA.$h.signature" "$h" "$o"
