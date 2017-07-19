10567f235f96b256b761ced242daaa9998b5340c
tedgoldstein <tedgoldstein@gmail.com>
2017-07-18 18:28:09 -0700

    cp showing

--- shiny/server.R
+++ shiny/server.R
@@ -1,11 +1,17 @@
+
+firstTime = TRUE
+
+
 function(input, output, session) {
   
-  testdata = read.table("testdata", row.names=1, header=TRUE)
-  output$radarchart <- renderRadarChart({
-    # Return a data frame. Each column will be a series in the hallmark radar chart.
-      cat("hello");
-      data.frame(testdata)
-  })
+  if (firstTime) {
+      firstTime = FALSE
+      testdata = read.table("testdata", row.names=1, header=TRUE)
+      output$radarchart <- renderRadarChart({
+        # Return a data frame. Each column will be a series in the hallmark radar chart.
+          data.frame(testdata)
+      })
+}
 
 
 
@@ -17,15 +23,31 @@ function(input, output, session) {
       }
 
   })
-  
   observeEvent( input$study, {
     if (input$study != "All") {
       m <- Metadata[grep(input$study, Metadata$Study),];
       filename = m[1,"File"]
-      SampleData <- read.table(paste0("datasets/", filename), header=TRUE, row.names=1, sep="\t");
-      updateSelectInput(session, "sample", choices = row.names(SampleData));
+      if (filename != "N/A") {
+          sampleData <- read.table(paste0("datasets/", filename), header=TRUE, row.names=1, sep="\t");
+          updateSelectInput(session, "sample", choices = row.names(sampleData));
+      }
     }
-    
   })
 
+  
+  observeEvent( input$sample, {
+    if (input$sample != "N/A") {
+      m <- Metadata[grep(input$study, Metadata$Study),];
+      filename = m[1,"File"]
+      if (filename != "N/A") {
+          sampleData <- read.table(paste0("datasets/", filename), header=TRUE, row.names=1, sep="\t");
+          df <- sampleData[input$sample, ];
+          output$radarchart <- renderRadarChart({
+            # Return a data frame. Each column will be a series in the hallmark radar chart.
+              browser();
+              df
+          })
+      }
+    }
+  })
 }
