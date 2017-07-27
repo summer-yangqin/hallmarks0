a09730e5d6f99bbee2e59a04c701c2fe3cc68799
tedgoldstein <tedgoldstein@gmail.com>
2017-07-27 11:35:00 -0700

    Simplification

--- Signatures/bin/buildSignatures.sh
+++ Signatures/bin/buildSignatures.sh
@@ -9,39 +9,39 @@ mkdir -p $s $t
 while read p q; do
 
   for h in `ls $i/`; do
-    echo Hallmark $h
-    echo p $p
-    echo q $q
-
     if [ -f "$s/$p.to.$q.$h.signature" ]; 
     then
-        echo "$s/$p.to.$q.$h.signature" exists 
+        # shhh
+        echo "$s/$p.to.$q.$h.signature" exists  > /dev/null  
     else
         echo making "$s/$p.to.$q.$h.signature" 
 
-        # echo python2 bin/matrix_slice.py $i/$h "lists/TCGA.$q"  ReferenceData/TCGA.RSEM "$t/$q.$h.data"
-        python2 bin/matrix_slice.py $i/$h "lists/TCGA.$q"  ReferenceData/TCGA.RSEM > "$t/$q.$h.data"
 
-        # echo python2 bin/matrix_slice.py $i/$h "lists/GTEX.$p"  ReferenceData/GTEX.RSEM 
-        python2 bin/matrix_slice.py $i/$h "lists/GTEX.$p"  ReferenceData/GTEX.RSEM > "$t/$p.$h.data"
+        if [ "X$1" != "Xdiagnostic" ]; then
+            # echo python2 bin/matrix_slice.py $i/$h "lists/TCGA.$q"  ReferenceData/TCGA.RSEM "$t/$q.$h.data"
+            python2 bin/matrix_slice.py $i/$h "lists/TCGA.$q"  ReferenceData/TCGA.RSEM > "$t/$q.$h.data"
 
-        # echo python2 bin/matrix_join_common.py "$t/$p.$h.data" "$t/$q.$h.data" "$t/$p.to.$q.$h.data"
-        python2 bin/matrix_join_common.py "$t/$p.$h.data" "$t/$q.$h.data" > "$t/$p.to.$q.$h.data"
+            # echo python2 bin/matrix_slice.py $i/$h "lists/GTEX.$p"  ReferenceData/GTEX.RSEM 
+            python2 bin/matrix_slice.py $i/$h "lists/GTEX.$p"  ReferenceData/GTEX.RSEM > "$t/$p.$h.data"
 
-        # echo Rscript bin/thresholdColumns.R "$t/$p.to.$q.$h.data"
-        Rscript bin/thresholdColumns.R "$t/$p.to.$q.$h.data"
+            # echo python2 bin/matrix_join_common.py "$t/$p.$h.data" "$t/$q.$h.data" "$t/$p.to.$q.$h.data"
+            python2 bin/matrix_join_common.py "$t/$p.$h.data" "$t/$q.$h.data" > "$t/$p.to.$q.$h.data"
 
-        # echo head -1 "$t/$p.to.$q.$h.data" 
-        head -1 "$t/$p.to.$q.$h.data" | tr '\t' '\n' | sed -e '1d;s/GTEX.*/0/;s/TCGA.*/1/' > "$t/$p.to.$q.$h.phen"
+            # echo Rscript bin/thresholdColumns.R "$t/$p.to.$q.$h.data"
+            Rscript bin/thresholdColumns.R "$t/$p.to.$q.$h.data"
 
-        # echo cut -f1 "$t/$p.to.$q.$h.data"
-        cut -f1 "$t/$p.to.$q.$h.data" | sed -e '1d' > "$t/$p.to.$q.$h.genes"
+            # echo head -1 "$t/$p.to.$q.$h.data" 
+            head -1 "$t/$p.to.$q.$h.data" | tr '\t' '\n' | sed -e '1d;s/GTEX.*/0/;s/TCGA.*/1/' > "$t/$p.to.$q.$h.phen"
 
-        # echo python2 bin/adjacency.py  ReferenceData/NETWORK "$t/$p.to.$q.$h.genes" 
-        python2 bin/adjacency.py  ReferenceData/NETWORK "$t/$p.to.$q.$h.genes" | sed -e 's/ //g' > "$t/$p.to.$q.$h.network"
+            # echo cut -f1 "$t/$p.to.$q.$h.data"
+            cut -f1 "$t/$p.to.$q.$h.data" | sed -e '1d' > "$t/$p.to.$q.$h.genes"
 
-        # echo Rscript bin/gelNet.R "$t/$p.to.$q.$h.data" "$t/$p.to.$q.$h.phen" "$t/$p.to.$q.$h.network" "$s/$p.to.$q.$h_$h.signature" "$h" "$p" "$q"
-        Rscript bin/gelNet.R "$t/$p.to.$q.$h.data" "$t/$p.to.$q.$h.phen" "$t/$p.to.$q.$h.network" "$s/$p.to.$q.$h.signature" "$h" "$p" "$q"
+            # echo python2 bin/adjacency.py  ReferenceData/NETWORK "$t/$p.to.$q.$h.genes" 
+            python2 bin/adjacency.py  ReferenceData/NETWORK "$t/$p.to.$q.$h.genes" | sed -e 's/ //g' > "$t/$p.to.$q.$h.network"
+
+            # echo Rscript bin/gelNet.R "$t/$p.to.$q.$h.data" "$t/$p.to.$q.$h.phen" "$t/$p.to.$q.$h.network" "$s/$p.to.$q.$h_$h.signature" "$h" "$p" "$q"
+            Rscript bin/gelNet.R "$t/$p.to.$q.$h.data" "$t/$p.to.$q.$h.phen" "$t/$p.to.$q.$h.network" "$s/$p.to.$q.$h.signature" "$h" "$p" "$q"
+        fi
     fi
 
   done
