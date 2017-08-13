b09e89c70cd6ff9fb0a65ec795d5251fa480765e
tedgoldstein <tedgoldstein@gmail.com>
2017-08-13 10:01:26 -0700

    cp

--- Shiny/ui.R
+++ Shiny/ui.R
@@ -1,21 +1,30 @@
 library(shiny)
+
+libraryPanel <- function()  
+    div(style="display:inline-block",
+        selectInput('cancer', 'Cancers', Cancers),
+        selectInput('study',  'Studies', Studies),
+        selectInput('filter', 'Filter Based on the following terms', NULL, multiple=TRUE, selectize=TRUE)
+    )
+
+
+
 function(request) {
 
     fluidPage(
       tags$h2("Oncology Model Fidelity Score"),
       fluidRow(
         column(width=3, 
-            div(style="display:inline-block",
-                selectInput('cancer', 'Cancers', Cancers),
-                selectInput('study',  'Studies', Studies),
-                selectInput('filter', 'Filter Based on the following terms', NULL, multiple=TRUE, selectize=TRUE),
-                    checkboxInput("showOnlySelectedSamples", "Show only selected samples", FALSE)),
-                    checkboxInput("zodiac", "Hallmark Zodiac", value = TRUE, width = NULL),
-                    bookmarkButton()),
-          column(width=9, 
-            rHandsontableOutput("hot"))),
+                    libraryPanel(),
+                    bookmarkButton()
+        ),
+        column(width=9, 
+            checkboxInput("showOnlySelectedSamples", "Show only selected samples", FALSE),
+            rHandsontableOutput("hot"))
+      ),
       fluidRow(
             column(width=7,
+                 checkboxInput("zodiac", "Hallmark Zodiac", value = TRUE, width = NULL),
                 radarChartOutput("radarchart")),
             column(width=5,
                 tags$div(class="legend-div",
