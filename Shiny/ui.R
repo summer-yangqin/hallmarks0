a4f73237842e3ae77c8516807aedb0df49674ca6
tedgoldstein <tedgoldstein@gmail.com>
2017-08-13 10:13:20 -0700

    cp

--- Shiny/ui.R
+++ Shiny/ui.R
@@ -7,11 +7,45 @@ libraryPanel <- function()
         selectInput('filter', 'Filter Based on the following terms', NULL, multiple=TRUE, selectize=TRUE)
     )
 
+uploadPanel <- function() 
+   wellPanel(
+      fileInput('file1', 'Choose file to upload',
+                accept = c(
+                  'text/csv',
+                  'text/comma-separated-values',
+                  'text/tab-separated-values',
+                  'text/plain',
+                  '.csv',
+                  '.tsv'
+                )
+      ),
+      tags$hr(),
+      checkboxInput('header', 'Header', TRUE),
+      radioButtons('sep', 'Separator',
+                   c(Comma=',',
+                     Semicolon=';',
+                     Tab='\t'),
+                   ','),
+      radioButtons('quote', 'Quote',
+                   c(None='',
+                     'Double Quote'='"',
+                     'Single Quote'="'"),
+                   '"'),
+      tags$hr(),
+      p('If you want a sample .csv or .tsv file to upload,',
+        'you can first download the sample',
+        a(href = 'mtcars.csv', 'mtcars.csv'), 'or',
+        a(href = 'pressure.tsv', 'pressure.tsv'),
+        'files, and then try uploading them.'
+      )
+  )
 
 
 function(request) {
 
     fluidPage(
+      tags$style("input[type=checkbox] { transform: scale(1.8); }"),
+
       fluidRow(
            column(width=5, tags$h2("Oncology Model Fidelity Score")),
            column(width=5, bookmarkButton())
@@ -19,7 +53,8 @@ function(request) {
       fluidRow( 
         column(width=3, 
             tabsetPanel(
-                tabPanel("Library", libraryPanel())
+                tabPanel("Library", libraryPanel()),
+                tabPanel("Upload", uploadPanel())
             )
         ),
         column(width=9, 
